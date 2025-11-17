24fedb129d46512e7de6a7e586ac213790debfe9
Liberian Post <info@liberianpost.com>
2025-11-17 18:16:03 +0100

    Create useForm.js

---
+++ src/hooks/useForm.js
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+export const useForm = (initialState = {}) => {
+  const [formData, setFormData] = useState(initialState)
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setFormData(prev => ({
+      ...prev,
+      [name]: value
+    }))
+  }
+
+  const resetForm = () => {
+    setFormData(initialState)
+  }
+
+  return {
+    formData,
+    handleChange,
+    resetForm,
+    setFormData
+  }
+}
