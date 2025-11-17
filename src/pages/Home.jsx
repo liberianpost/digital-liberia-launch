89f3c1b457e56e22b64d6795917028c320a9d209
Liberian Post <info@liberianpost.com>
2025-11-17 18:17:42 +0100

    Create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Hero from '../components/Hero/Hero'
+import Events from '../components/Events/Events'
+import FocusAreas from '../components/FocusAreas/FocusAreas'
+import Contact from '../components/Contact/Contact'
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <Events />
+      <FocusAreas />
+      <Contact />
+    </>
+  )
+}
+
+export default Home
